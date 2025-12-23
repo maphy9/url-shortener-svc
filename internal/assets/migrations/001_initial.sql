b15c7ccbbcdce1da0246c19b5248351158b4b693
maphy9 <maphy9@gmail.com>
2025-12-23 14:38:30 +0100

    Add very basic url shortening

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -1,10 +1,12 @@
 -- +migrate Up
-CREATE TABLE URL_MAPPING
+DROP TABLE IF EXISTS url_mapping;
+
+CREATE TABLE url_mapping
 (
-    ORIGINAL_URL TEXT NOT NULL,
-    SHORTENED_URL TEXT NOT NULL,
-    CREATED_AT TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    original_url TEXT NOT NULL UNIQUE,
+    shortened_url TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT current_timestamp
 );
 
-CREATE INDEX IF NOT EXISTS URL_MAPPING_INDEX ON URL_MAPPING(ORIGINAL_URL, SHORTENED_URL);
+CREATE INDEX IF NOT EXISTS url_mapping_index ON url_mapping(original_url, shortened_url);
 -- +migrate Down
--- internal/service/handlers/shorten.go
+++ internal/service/handlers/shorten.go
@@ -3,8 +3,10 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"math/big"
 	"net/http"
 	"net/url"
+	"time"
 )
 
 type ShortenURLRequest struct {
@@ -19,15 +21,22 @@ func (r ShortenURLRequest) IsValid() bool {
     return err == nil && u.Host != ""
 }
 
-func (r ShortenURLRequest) ToSql() (string, []interface{}, error) {
-	return fmt.Sprintf(`
-		SELECT COUNT(*)
-		FROM URL_MAPPING
-		WHERE ORIGINAL_URL='%s';`, r.URL), []interface{}{}, nil
+type Query struct {
+	SQL string
+	Args []interface{}
+}
+
+func (q Query) ToSql() (string, []interface{}, error) {
+	return q.SQL, q.Args, nil
+}
+
+type URLMapping struct {
+	OriginalURL string		`db:"original_url" json:"original_url"`
+	ShortenedURL string		`db:"shortened_url" json:"shortened_url"`
+	CreatedAt time.Time		`db:"created_at" json:"created_at"`
 }
 
 func ShortenURL(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("URL: %v, Method: %v\n", r.URL, r.Method)
 	var body ShortenURLRequest
 	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
 		http.Error(w, "Bad request body", http.StatusBadRequest)
@@ -38,13 +47,41 @@ func ShortenURL(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	db := DB(r)
-	res, err := db.ExecWithResult(body)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Internal server error: %v", err), http.StatusInternalServerError)
+	var shortenedUrl string
+	if err := db.Get(&shortenedUrl, Query{
+		SQL: `SELECT shortened_url FROM url_mapping
+		WHERE original_url = $1;`,
+		Args: []interface{} { body.URL },
+	}); err == nil {
+		w.Write([]byte("http://localhost" + r.RequestURI + "/" + shortenedUrl))
+		return
 	}
-	rowsAffected, err := res.RowsAffected()
-	if err != nil {
+
+	// TODO: Refactor numbering logic
+	var countString string
+	if err := db.Get(
+		&countString,
+		Query{SQL: "SELECT COUNT(*) FROM url_mapping"},
+	); err != nil {
 		http.Error(w, fmt.Sprintf("Internal server error: %v", err), http.StatusInternalServerError)
+		return
 	}
-	w.Write([]byte(fmt.Sprintf("Rows affected: %d", rowsAffected)))
+	count := new(big.Int)
+	count, _ = count.SetString(countString, 10)
+	if count == nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+	count.Add(count, new(big.Int).SetInt64(1))
+	base62 := count.Text(62)
+	if err := db.Exec(Query{
+		SQL: `INSERT INTO url_mapping(
+			original_url, shortened_url
+		) VALUES($1, $2);`,
+		Args: []interface{} {body.URL, base62},
+	}); err != nil {
+		http.Error(w, fmt.Sprintf("Internal server error: %v", err), http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte("http://localhost" + r.RequestURI + "/" + base62))
 }
