5b84d44918dfb258c5dfffc53416bbf496ae32b9
maphy9 <maphy9@gmail.com>
2025-12-23 09:02:44 +0100

    Add url validation

--- internal/service/handlers/shorten.go
+++ internal/service/handlers/shorten.go
@@ -4,22 +4,36 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 type ShortenURLRequest struct {
 	URL string `json:"url"`
 }
 
-func (r ShortenURLRequest) String() string {
-	return fmt.Sprintf("URL: %s", r.URL)
+func (r ShortenURLRequest) IsValid() bool {
+    u, err := url.Parse(r.URL)
+	if u.Scheme == "" {
+		u.Scheme = "https"
+	}
+    return err == nil && u.Host != ""
 }
 
 func ShortenURL(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("URL: %v, Method: %v\n", r.URL, r.Method)
 	var body ShortenURLRequest
 	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
-		fmt.Errorf("%v\n", err)
+		http.Error(w, "Bad request body", http.StatusBadRequest)
+		return
+	}	
+	if !body.IsValid() {
+		http.Error(w, "Bad url", http.StatusBadRequest)
 		return
 	}
-	w.Write([]byte(body.String()))
+	res, err := json.Marshal(body)
+	if err != nil {
+		http.Error(w, "I am a teapot", http.StatusInternalServerError)
+		return
+	}
+	w.Write(res)
 }
